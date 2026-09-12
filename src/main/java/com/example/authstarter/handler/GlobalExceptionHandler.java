eb79d8a467d98ddcfeb3adb908a15ab2b1725fab
Peter Nwaogu <142111670+Peter19570@users.noreply.github.com>
2026-09-12 18:54:31 +0000

    feat: Add AuthorizationDeniedException to GlobalExceptionHandler for improved authentication error handling

--- src/main/java/com/example/authstarter/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/authstarter/handler/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -48,7 +49,8 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({
             BadCredentialsException.class,
             UsernameNotFoundException.class,
-            AccessDeniedException.class
+            AccessDeniedException.class,
+            AuthorizationDeniedException.class
     })
     public ResponseEntity<ApiResponse<String>> handleAuthException(Exception ex){
         return ResponseEntity
